2ecf8ccf6462fe97dc422ca5ff10dccc93b6571a
anshu-kumar27 <96561295+anshu-kumar27@users.noreply.github.com>
2023-09-04 00:06:29 +0530

    Add files via upload

---
+++ details.js
@@ -0,0 +1,13 @@
+//22:50  Setting up backend
+// 37:49  Making product APIs
+// 1:02:35 Backend error handling
+// 1:24:23 Search, filter, pagination
+// 1:52:18 Backend user & password authentication
+// 3:11:58
+// 3:25:19 Backend user routes APIs
+// 3:39:50
+// 4:08:29 Making order APIs
+// 4:42:59 Starting with frontend
+// 4:47:56
+// 5:18:03 start implementing Redux
+
